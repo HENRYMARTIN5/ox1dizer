c2dcb8c3f7ae1f3617de0082a365da8d3c3c7aaa
coredoescode <36366790+coredoescode@users.noreply.github.com>
2021-04-07 12:16:22 -0400

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,10 @@
+# the ox1dizer project
+# Nothing here yet except logger setup and POC console check
+
+def info(msg, thread):
+  print("[ox1dizer/" + thread
+
+def ox1_banner_poc_console():
+  print("the ox1dizer project - initializing")
+  # todo: init function
+ 
